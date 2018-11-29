e2e099b8925af373a55415648f720deeaa15c38f
AnghelLeonard <leoprivacy@yahoo.com>
2018-11-29 13:39:22 +0200

    Update Main.java

--- stack/StackViaConcurrentLinkedDeque/src/main/java/ds/stackviaconcurrentlinkeddeque/Main.java
+++ stack/StackViaConcurrentLinkedDeque/src/main/java/ds/stackviaconcurrentlinkeddeque/Main.java
@@ -2,20 +2,58 @@ package ds.stackviaconcurrentlinkeddeque;
 
 import java.util.Deque;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        
-        // A ConcurrentLinkedDeque implementation can be used as a Stack (Last-In-First-Out) 
-        Deque<String> stack = new ConcurrentLinkedDeque<>();
-        
-        stack.push("Rafael Nadal");
-        stack.push("Roger Federer");
-        stack.push("David Ferer");
-        
-        System.out.println("First in stack: " + stack.getFirst()); // David Ferer
-        System.out.println("Pop from stack: " + stack.pop()); // David Ferer
-    }    
-}
 
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    // A ConcurrentLinkedDeque implementation can be used as a Stack (Last-In-First-Out) 
+    // Switch to ArrayDeque to see how polling is trying to poll 
+    // the same item in different threads because ArrayDeque is not blocking
+    private static Deque<Integer> stack = new ConcurrentLinkedDeque<>();
+
+    public static void main(String[] args) {
+
+        logger.info("Push ...");
+
+        // push values for 0 to 10 using a single thread
+        for (int i = 0; i < 10; i++) {
+            logger.log(Level.INFO,
+                    "Push item{0},[{1}]",
+                    new Object[]{i, Thread.currentThread().getName()});
+
+            stack.push(i);
+        }
+
+        logger.info("Pop ... ");
+
+        // pop values using 5 threads
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            executor.execute(() -> {
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+
+                logger.log(Level.INFO,
+                        "Pop item {0}, [{1}]",
+                        new Object[]{stack.pop(), Thread.currentThread().getName()});
+            });
+        }
+
+        executor.shutdown();
+
+        while (!executor.isTerminated()) {
+        }
+
+        logger.info("Finished all threads");
+
+    }
+}
