e3fa4d30051ba528351756ff4cd9c3e4de9ca3c4
AnghelLeonard <leoprivacy@yahoo.com>
2018-11-30 10:57:21 +0200

    Add files via upload

---
+++ delayqueue/QueueViaDelayQueue/src/main/java/ds/queueviadelayqueue/Main.java
@@ -0,0 +1,43 @@
+package ds.queueviadelayqueue;
+
+import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.DelayQueue;
+
+public class Main {
+    
+    public static void main(String[] args) throws InterruptedException {
+        
+        final long LOS_ANGELES_DELAY_MS = 10000L; // delay 10s
+        final long DETROIT_DELAY_MS = 15000L; // delay 15s        
+        final long HUSTON_DELAY_MS = 30000L; // delay 30s
+        
+        final long WAIT_MS = 16000; // wait 16s
+        
+        BlockingQueue<TrainDelay> queue = new DelayQueue<>();
+        
+        // produce 3 trains with different delays 
+        TrainDelay trainToDetroit = 
+                new TrainDelay(UUID.randomUUID().toString(), "Detroit", DETROIT_DELAY_MS);
+        TrainDelay trainToLosAngeles = 
+                new TrainDelay(UUID.randomUUID().toString(), "Los Angeles", LOS_ANGELES_DELAY_MS);
+        TrainDelay trainToHuston = 
+                new TrainDelay(UUID.randomUUID().toString(), "Huston", HUSTON_DELAY_MS);
+        
+        // enqueue the trains order ascending by delay
+        // the order is ensured by the TrainDelay#compareTo() implementation
+        queue.offer(trainToDetroit); // delay 15s               
+        queue.offer(trainToLosAngeles); // delay: 10s   
+        queue.offer(trainToHuston); // delay 30s                  
+        
+        // consume trains, we arbitrary decided to wait 16s until polling
+        // after 16s, we can access the Los Angeles and Detroit trains, 
+        // but not the Huston (this train still has 14s delay)
+        Thread.sleep(WAIT_MS);
+        
+        System.out.println("Train: " + queue.poll()); // Los Angeles        
+        System.out.println("Train: " + queue.poll()); // Detroit
+        System.out.println("Train: " + queue.poll()); // null
+    }
+    
+}
