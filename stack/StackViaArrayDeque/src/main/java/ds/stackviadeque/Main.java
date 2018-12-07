765b3979ef7b9ada564cc77c7e1b83f084bbf05d
AnghelLeonard <leoprivacy@yahoo.com>
2018-12-07 14:16:17 +0200

    Update Main.java

--- stack/StackViaArrayDeque/src/main/java/ds/stackviadeque/Main.java
+++ stack/StackViaArrayDeque/src/main/java/ds/stackviadeque/Main.java
@@ -7,14 +7,18 @@ public class Main {
     
     public static void main(String[] args) {
         
-        // An ArrayDeque implementation can be used as a Stack (Last-In-First-Out) 
+        // An ArrayDeque implementation can be used as a Stack (Last-In-First-Out, LIFO) 
         Deque<String> stack = new ArrayDeque<>();
         
         stack.push("Rafael Nadal");
         stack.push("Roger Federer");
         stack.push("David Ferer");
         
-        System.out.println("First in stack: " + stack.getFirst());
-        System.out.println("Pop from stack: " + stack.pop());
+        // Retrieves, but does not remove, the first element of this deque
+        System.out.println("First in stack: " + stack.getFirst());       
+        
+        // Pops an element from the stack represented by this deque. 
+        // In other words, removes and returns the first element of this deque.
+        System.out.println("Pop from stack: " + stack.pop()); 
     }    
 }
